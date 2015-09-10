29cf5a05bce2ee24d15372566bf223a1312dbff2
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2015-09-10 20:00:09 +0100

    modified associations for pg

--- db/migrate/20150905234637_remove_user_column_from_reservations_table.rb
+++ db/migrate/20150905234637_remove_user_column_from_reservations_table.rb
@@ -1,6 +1,5 @@
 class RemoveUserColumnFromReservationsTable < ActiveRecord::Migration
   def change
-    remove_column :reservations, :user_id
     add_column :bookings, :user_id, :integer
   end
 end
