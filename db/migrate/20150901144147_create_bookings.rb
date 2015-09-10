c2d09df5823e68f0d56dab2f8f83f1fcab6d76e7
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2015-09-10 19:53:47 +0100

    modified associations for pg

--- db/migrate/20150901144147_create_bookings.rb
+++ db/migrate/20150901144147_create_bookings.rb
@@ -1,7 +1,7 @@
 class CreateBookings < ActiveRecord::Migration
   def change
     create_table :bookings do |t|
-      t.references :passenger, index: true, foreign_key: true
+      t.references :passenger, index: true
       t.datetime :booking_date
       t.integer :booking_price
 
