5708c42a98b825433b3832ad3a91323899c673e4
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2015-09-10 19:47:48 +0100

    modified associations for pg

--- db/migrate/20150901143943_create_flights.rb
+++ db/migrate/20150901143943_create_flights.rb
@@ -1,8 +1,8 @@
 class CreateFlights < ActiveRecord::Migration
   def change
     create_table :flights do |t|
-      t.references :origin, index: true, foreign_key: true
-      t.references :destination, index: true, foreign_key: true
+      t.references :origin, index: true
+      t.references :destination, index: true
       t.datetime :departure_date
       t.datetime :arrival_date
 
