181726253fd9a860dfb3260bdd79b61a13a6b5c1
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2015-09-15 16:55:15 +0100

    added url routers for routes not matched

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,30 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
+
+  rescue_from ::ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ::NameError, with: :error_occurred
+  rescue_from ::ActionController::RoutingError, with: :no_route_found
+  rescue_from ::Exception, with: :error_occurred
+
+
+  def no_route_found
+    flash[:danger] = "You entered an invalid address!"
+    redirect_to root_path
+  end
+
 private
+  def record_not_found(exception)
+    flash[:danger] = exception.message.to_s
+    redirect_to root_path
+  end
+
+
+  def error_occurred(exception)
+    flash[:danger] = exception.message.to_s
+    redirect_to root_path
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]).decorate if session[:user_id]
   end
